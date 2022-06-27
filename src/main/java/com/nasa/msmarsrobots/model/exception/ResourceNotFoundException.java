49329ee6344c01151b99e152111b38aebaa48f1a
Kauai Guarilha <kauai.guarilha@hotmail.com>
2022-06-27 11:21:36 -0300

    Alteration on response status at ResourceNotFoundException.class

--- src/main/java/com/nasa/msmarsrobots/model/exception/ResourceNotFoundException.java
+++ src/main/java/com/nasa/msmarsrobots/model/exception/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ import com.nasa.msmarsrobots.model.domain.EValidation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class ResourceNotFoundException extends ExceptionAbstract {
 
     public ResourceNotFoundException(EValidation validation, String... params) {
